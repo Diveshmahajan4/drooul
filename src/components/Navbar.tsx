96b06274f92a95e3ca01d190e963eca9237b7013
DiveshMahajan <diveshmahajan04@gmail.com>
2023-12-21 22:23:20 +0530

    navbar added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,56 @@
+import Link from "next/link"
+import Image from "next/image"
 import MaxWidthWrapper from "./MaxWidthWrapper"
+import NavItems from "./NavItems"
+import { buttonVariants } from "./ui/button"
+import Cart from "./Cart"
 
 
 const Navbar = () => {
+    const user = null;
   return (
-    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
-        <header className="relative bg-white">
-            <MaxWidthWrapper className="border-b border-gray-200">
-                <div className="flex h-16 items-center">
-                    {/* Mobile Navigation */}
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16 ">
+        <header className="relative bg-white border-b border-gray-200">
+            <MaxWidthWrapper>
+                <div className="border-b border-gray-200">
+                    <div className="flex h-16 items-center">
+                        {/* Mobile Navigation */}
+                        <div className="ml-4 flex lg:ml-0">
+                            <Link href='/'>
+                                <img src="./logoblue.png" alt="logo" className="w-48 h-48" />
+                            </Link>
+                        </div>
+
+                        <div className="hidden z-50 lg:ml-0 lg:block lg:self-stretch">
+                            <NavItems/>
+                        </div>
+
+                        <div className="ml-auto flex items-center">
+                            <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6 ">
+                                {user ? null : <Link href='/sign-in' className={buttonVariants({variant: 'outline',})}>Sign In</Link>}
+                            
+
+                                {user ? null : (
+                                    <span className="h-6 w-px bg-gray-200" aria-hidden='true'/>
+                                )}
+
+                                {user ? (<p></p>) : (<Link href='/sign-up' className={buttonVariants({variant: 'outline'})}> Create Account</Link>)}
+
+                                {user ? <span className="h-6 w-px bg-gray-200" aria-hidden='true'/> : null}
+
+                                {user ? null : (
+                                    <div className="flex lg:ml-6">
+                                        <span className="h-6 w-px bg-gray-200" aria-hidden='true'/>
+                                    </div> 
+                                )}
+                                <div className="ml-4 flow-root lg:ml-6">
+                                    <Cart/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                
             </MaxWidthWrapper>
         </header>
     </div>
