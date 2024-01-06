a4bb08925d70870c366f01c065db7f386ee48af2
DiveshMahajan <diveshmahajan04@gmail.com>
2024-01-06 10:46:59 +0530

    Minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,15 +21,14 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn('relative h-full font-sans antialiased', inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
-          <Gradient>
             <Providers>
             <Navbar/>
-            <div className='flex-grow flex-1'>
+              <div className='flex-grow flex-1'>
               {children}
-            </div>
+              </div>
             <Footer/>
+            
           </Providers>
-          </Gradient>
           
         </main>
         <Toaster position='top-center' richColors/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react'
 import ProductReel from '@/components/ProductReel'
+import Gradient from '@/components/Gradient'
 
 const perks = [
   {
@@ -28,7 +29,7 @@ export default function Home() {
   return (
     <>
     <MaxWidthWrapper>
-      <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
+        <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl '>
         <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl '>
           Your  marketplace for high-quality {" "} 
           <span className='text-blue-600'>
@@ -36,7 +37,7 @@ export default function Home() {
           </span> 
         </h1>
         <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
-          Welcome to Gesso. Every asset on our platform is verified by our team to ensure our highest quality standards.
+          Welcome to drool. Every asset on our platform is verified by our team to ensure our highest quality standards.
         </p>
         <div className='flex flex-col sm:flex-row gap-4 mt-6'>
           <Link href='./products' className={buttonVariants()}>Browse Trending</Link>
